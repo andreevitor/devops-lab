e9b812bbda842df0a6be60cc47b19926bc143031
Andre <andrevitorufpr@gmail.com>
2026-04-19 22:17:13 -0300

    subtracao

--- app.py
+++ app.py
@@ -4,12 +4,3 @@ def soma(a, b):
 print("Resultado:", soma(2, 3))
 def subtracao(a, b):
     return a - b
-def multiplicacao(a, b):
-    return a * b
-def divisao(a, b):
-    if b == 0:
-        return "erro"
-    return a / b
-print(subtracao(5,2))
-print(multiplicacao(3,4))
-print(divisao(10,2))
